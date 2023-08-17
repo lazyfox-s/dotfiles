f46b905e49e72db3acf39b4fb12c5654a3da09a8
lazyfox <135625123+lazyfox-s@users.noreply.github.com>
2023-08-17 18:15:08 +0900

    add neovim setting

---
+++ dot_config/nvim/lua/base.lua
@@ -0,0 +1,53 @@
+-- Encoding
+vim.o.encoding = 'utf-8'
+vim.o.fileencoding = 'utf-8'
+vim.scriptencoding = 'utf-8'
+vim.o.ambiwidth = 'single'
+
+
+-- Visual
+vim.o.number = true
+vim.o.visualbell = true
+vim.o.errorbells = false
+vim.o.cursorline = true
+
+--- indent
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+--- floating window
+vim.o.pumblend = 0
+vim.o.winblend = 0
+
+--- conceal
+vim.o.conceallevel = 2
+vim.o.concealcursor = ''
+vim.g.tex_conceal = 'adgmb'
+
+--- color
+vim.o.background = 'dark'
+vim.o.termguicolors = true
+
+-- tab line / status line
+vim.o.laststatus = 3
+vim.o.showtabline = 2
+
+
+-- search
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
+
+
+-- manipulation
+vim.opt.clipboard:append{'unnamedplus'}
+vim.o.timeout = true
+vim.o.timeoutlen = 500
+
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath('cache')..'/undo'
---
+++ dot_config/nvim/lua/keybind.lua
@@ -0,0 +1,39 @@
+-- cursor motion
+vim.keymap.set({'n', 'v'}, 'j', 'gj')
+vim.keymap.set({'n', 'v'}, 'k', 'gk')
+vim.keymap.set({'n', 'v'}, '<Down>', 'gj')
+vim.keymap.set({'n', 'v'}, '<UP>', 'gk')
+
+vim.keymap.set({'n', 'v'}, '<C-h>', 'g^')
+vim.keymap.set({'n', 'v'}, '<C-l>', 'g$')
+vim.keymap.set('i', '<C-h>', '<C-o><C-h>', {remap = true})
+vim.keymap.set('i', '<C-l>', '<C-o><C-l>', {remap = true})
+
+-- corresponds <C-[>
+vim.keymap.set('i', '<C-]>', '<ESC><Right>')
+
+vim.keymap.set({'n', 'v'}, ';', ':')
+vim.keymap.set({'n', 'v'}, ':', ';')
+
+-- inspired by C or D
+vim.keymap.set('n', 'Y', 'y$')
+
+-- do not overwrite resister by x
+vim.keymap.set('n', 'x', '"_x')
+
+-- <SPACE to toggle folding
+vim.keymap.set('n', '<SPACE>', 'za')
+
+-- <ESC> to normal mode in terminal
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>')
+
+
+-- buffer
+vim.keymap.set('n', 'bn', vim.cmd.bnext)
+vim.keymap.set('n', 'bN', vim.cmd.bbprevious)
+vim.keymap.set('n', 'bq', vim.cmd.bdelete)
+vim.keymap.set('n', 'bd', function ()
+    local buf = vim.api.nvim_get_current_buf()
+    vim.cmd.bprevious()
+    vim.api.nvim_buf_delete(buf, {})
+end)
