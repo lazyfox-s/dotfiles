2a44a8cdab5d314d20466aee1933ff692db32e14
lazyfox <135625123+lazyfox-s@users.noreply.github.com>
2023-08-17 16:52:07 +0900

    add fish settings

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,40 @@
+set -g fish_ambiguous_width 1
+set -g fish_term24bit 1 # true color
+set -g ENHANCD_DISABLE_DOT 1
+set -g ENHANCD_DISABLE_HOME 1
+
+abbr -a ls exa --icons
+abbr -a ll exa -l --icons
+abbr -a la exa -la --icons
+abbr -a lt exa --tree --icons
+abbr -a lta exa --tree -a --icons
+abbr -a cat bat
+abbr -a vim nvim
+
+set timg_option ''
+if [ $TERM = 'wezterm' ]
+    set timg_option "-pi"
+end
+
+abbr -a icat timg -g100x100 $timg_potion
+
+# color theme
+type scheme > /dev/null 2>&1 && scheme set tomorrow-night-bright
+
+starship init fish | source
+
+function fish_greeting
+    type macchina > /dev/null 2>&1 && macchina
+end
+
+function ils
+  if count $argv > /dev/null
+    timg -g100x100 --grid=5 --frames=1 --title $timg_option $argv
+  else
+    timg -g100x100 --grid=5 --frames=1 --title $timg_option *
+  end
+end
+
+function ranger
+    test -z $RANGER_LEVEL && command ranger $argv || exit
+end
